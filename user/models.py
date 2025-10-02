237c99b69fef9525906442850ca7bde49e333b6d
Nodara <nodo.gogaladze2002@gmail.com>
2025-10-02 17:33:49 +0000

    feat(auth): custom usermanager and user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,67 @@
+from typing import Any
+
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(
+        self, email: str, password: str | None = None, **extra_fields: Any
+    ) -> "User":
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self, email: str, password: str | None = None, **extra_fields: Any
+    ) -> "User":
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("superuser must have 'is_staff=True'.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("superuser must have 'is_superuser=True'.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    class Gender(models.TextChoices):
+        MALE = "MALE", "Male"
+        FEMALE = "FEMALE", "Female"
+
+    username = None  # Remove the username field
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(
+        max_length=20, choices=Gender.choices, null=True, blank=True
+    )
+    height_cm = models.PositiveIntegerField(null=True, blank=True)
+    weight_kg = models.FloatField(
+        null=True, blank=True
+    )  # weight maybe is not needed, it will be in Weightentry model but now it's ok
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS: list[str] = []
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+        ordering = ["email"]
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
