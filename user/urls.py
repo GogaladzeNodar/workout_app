3fc81305823b1aec16bb40d715a7169c3617c1e0
Nodara <nodo.gogaladze2002@gmail.com>
2025-10-02 21:17:32 +0000

    feat(auth): JWT authorization

---
+++ user/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+    TokenBlacklistView,
+)
+
+app_name = "user"
+
+urlpatterns = [
+    path("token/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
+]
