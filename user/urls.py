073f1c4cd78f0a85d351ba1ac679f42a472229e3
Nodara <nodo.gogaladze2002@gmail.com>
2025-10-07 21:53:33 +0000

    feat(auth): user registration

--- user/urls.py
+++ user/urls.py
@@ -6,10 +6,12 @@ from rest_framework_simplejwt.views import (
     TokenBlacklistView,
 )
 from common.logging.view_part_logging.baseapiview import BaseTokenObtainPairView
+from .views import UserRegistrationView
 
 app_name = "user"
 
 urlpatterns = [
+    path("register/", UserRegistrationView.as_view(), name="user_register"),
     path("token/login/", BaseTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
--- user/views.py
+++ user/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from .serializers import UserRegistrationSerializer
+from rest_framework.views import APIView
+from common.logging.view_part_logging.baseapiview import BaseAPIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
 
 # Create your views here.
+
+
+class UserRegistrationView(BaseAPIView):
+    """
+    View to handle user registration.
+    """
+
+    serializer_class = UserRegistrationSerializer
+    permission_classes = []
+    authentication_classes = []
+
+    def get(self, request, *args, **kwargs):
+        serializer = self.serializer_class()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+
+            refresh = RefreshToken.for_user(user)
+
+            return Response(
+                {
+                    "message": "User registered successfully.",
+                    "user": {
+                        "id": user.id,
+                        "email": user.email,
+                    },
+                    "tokens": {
+                        "refresh": str(refresh),
+                        "access": str(refresh.access_token),
+                    },
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
